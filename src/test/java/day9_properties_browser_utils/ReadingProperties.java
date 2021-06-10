9a07b75e8b7332fe4b7860b4ebc7b8efc1bde461
VladimirAdamiya <adamiya.v@gmail.com>
2021-06-09 20:27:48 -0500

    initial commit

--- src/test/java/day9_properties_browser_utils/ReadingProperties.java
+++ src/test/java/day9_properties_browser_utils/ReadingProperties.java
@@ -1,6 +1,7 @@
 package day9_properties_browser_utils;
 
 import org.testng.annotations.Test;
+import utilities.ConfigurationReader;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -9,25 +10,33 @@ import java.util.Properties;
 
 public class ReadingProperties {
 
+
     @Test
     public void reading_from_properties_file() throws IOException {
 
-        // #1- Create object of Properties class (coming from Java Util)
+        //#1- Create object of Properties class (coming from Java lib)
 
         Properties properties = new Properties();
 
-        // #2- Open the file using FileInputStream
-        // We are trying to open a file, so we need to pass the path
+        //#2- Open the file using FileInputStream
+        //We are trying to open a file, so we need to pass the path.
 
         FileInputStream file = new FileInputStream("configuration.properties");
-
-        // #3- Load the properties object with our file
-
+        //
+        //#3- load the properties object with our file
         properties.load(file);
 
-        // reading from configuration.properties
+        //reading from configuration.properties
+        System.out.println("properties.getProperty(\"browser\") = " + properties.getProperty("browser"));
+        System.out.println("properties.getProperty(\"env\") = " + properties.getProperty("env"));
 
-        System.out.println("properties.get Property(\"browser\") = " + properties.getProperty("browser"));
-        System.out.println("properties.get Property(\"env\") = " + properties.getProperty("env"));
+    }
+
+
+    @Test
+    public void using_properties_method(){
+
+        System.out.println("ConfigurationReader.getProperty(\"browser\") = " + ConfigurationReader.getProperty("browser"));
+        System.out.println("ConfigurationReader.getProperty(\"env\") = " + ConfigurationReader.getProperty("env"));
     }
 }
